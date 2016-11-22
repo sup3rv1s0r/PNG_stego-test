af9cbb76d4eed9a9c89dbc04214480a725bd45ee
Junewoo Byeon <white_hat_security@nate.com>
2016-11-23 00:13:59 +0900

    base64 en/decode 함수 추가, 원본 코드의 goto문 제거

--- PNG_encode_decode/PNG_file.cpp
+++ PNG_encode_decode/PNG_file.cpp
@@ -48,6 +48,118 @@ unsigned int filesize(const char *filename)
 }
 
 
+/*
+*		MIME Base64 Algorithm
+*/
+
+/* Base64 Encode Table */
+static const char MimeBase64[] = {
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+	'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+	'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+	'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+	'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+	'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+	'w', 'x', 'y', 'z', '0', '1', '2', '3',
+	'4', '5', '6', '7', '8', '9', '+', '/'
+};
+
+/* Base64 Encode Table */
+static int DecodeMimeBase64[256] = {
+	-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,  /* 00-0F */
+	-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,  /* 10-1F */
+	-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,62,-1,-1,-1,63,  /* 20-2F */
+	52,53,54,55,56,57,58,59,60,61,-1,-1,-1,-1,-1,-1,  /* 30-3F */
+	-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,  /* 40-4F */
+	15,16,17,18,19,20,21,22,23,24,25,-1,-1,-1,-1,-1,  /* 50-5F */
+	-1,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,  /* 60-6F */
+	41,42,43,44,45,46,47,48,49,50,51,-1,-1,-1,-1,-1,  /* 70-7F */
+	-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,  /* 80-8F */
+	-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,  /* 90-9F */
+	-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,  /* A0-AF */
+	-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,  /* B0-BF */
+	-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,  /* C0-CF */
+	-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,  /* D0-DF */
+	-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,  /* E0-EF */
+	-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1   /* F0-FF */
+};
+
+/* Base64 Encode Function */
+int base64_encode(char *text, int numBytes, char **encodedText)
+{
+	unsigned char input[3]  = {0,0,0};
+	unsigned char output[4] = {0,0,0,0};
+	int   index, i, j, size;
+	char *p, *plen;
+	plen           = text + numBytes - 1;
+	size           = (4 * (numBytes / 3)) + (numBytes % 3? 4 : 0) + 1;
+	(*encodedText) = (char*)malloc(size);
+	j              = 0;
+	for  (i = 0, p = text;p <= plen; i++, p++) {
+		index = i % 3;
+		input[index] = *p;
+		if (index == 2 || p == plen) {
+			output[0] = ((input[0] & 0xFC) >> 2);
+			output[1] = ((input[0] & 0x3) << 4) | ((input[1] & 0xF0) >> 4);
+			output[2] = ((input[1] & 0xF) << 2) | ((input[2] & 0xC0) >> 6);
+			output[3] = (input[2] & 0x3F);
+			(*encodedText)[j++] = MimeBase64[output[0]];
+			(*encodedText)[j++] = MimeBase64[output[1]];
+			(*encodedText)[j++] = index == 0? '=' : MimeBase64[output[2]];
+			(*encodedText)[j++] = index <  2? '=' : MimeBase64[output[3]];
+			input[0] = input[1] = input[2] = 0;
+		}
+	}
+	(*encodedText)[j] = '\0';
+	return size;
+}
+
+/* Base64 Decode Function */
+int base64_decode(char *text, unsigned char *dst, int numBytes)
+{
+	const char* cp;
+	int space_idx = 0, phase;
+	int d, prev_d = 0;
+	unsigned char c;
+
+	space_idx = 0;
+	phase = 0;
+
+	for ( cp = text; *cp != '\0'; ++cp ) {
+		d = DecodeMimeBase64[(int) *cp];
+		if ( d != -1 ) {
+			switch ( phase ) {
+			case 0:
+				++phase;
+				break;
+			case 1:
+				c = ( ( prev_d << 2 ) | ( ( d & 0x30 ) >> 4 ) );
+				if ( space_idx < numBytes )
+					dst[space_idx++] = c;
+				++phase;
+				break;
+			case 2:
+				c = ( ( ( prev_d & 0xf ) << 4 ) | ( ( d & 0x3c ) >> 2 ) );
+				if ( space_idx < numBytes )
+					dst[space_idx++] = c;
+				++phase;
+				break;
+			case 3:
+				c = ( ( ( prev_d & 0x03 ) << 6 ) | d );
+				if ( space_idx < numBytes )
+					dst[space_idx++] = c;
+				phase = 0;
+				break;
+			}
+			prev_d = d;
+		}
+	}
+
+	return space_idx;
+
+}
+
+
 /* PNG Constructor
  * Constructor for the PNG_file class Simply reads in a PNG file
  */
@@ -132,35 +244,40 @@ void PNG_file::encode(const char *fileToEncodeName) {
 	//This section of code encodes the input file into the picture
 	//It encodes the input file bit by bit into the least significant
 	//bits of the original picture file
-	for(int y=0; y < read_ptr->height; y++) {
+	for(int y=0; y < read_ptr->height; y++)
+	{
 		int x=0;
 		//Write the file size into the file y==0 ensures that it only happens
 		//once
 		if(y == 0)
-			for(x; x < SIZE_WIDTH; x++) {
+			for(x; x < SIZE_WIDTH; x++)
+			{
 				if((size & ipow(2,x)))
 					*(row_pointers[y]+x) |= 1;
 				else
 					*(row_pointers[y]+x) &= 0xFE;
 			}
-		for(x; x < read_ptr->width*3; x++) {
-			if(x%BYTE_SIZE == 0) {
-				if(!fread(&buffer, 1, 1, fileToEncode)) 
-					goto loop_end;
+			for(x; x < read_ptr->width*3; x++)
+			{
+				if(x%BYTE_SIZE == 0)
+				{
+					if(!fread(&buffer, 1, 1, fileToEncode)) 
+						//goto loop_end;
+						break;
+				}
+				//png_bytep here = row_pointers[y]+x; for debugging
+				if((buffer & ipow(2,x%BYTE_SIZE)))
+					*(row_pointers[y]+x) |= 1;
+				else
+					*(row_pointers[y]+x) &= 0xFE;
 			}
-			//png_bytep here = row_pointers[y]+x; for debugging
-			if((buffer & ipow(2,x%BYTE_SIZE)))
-				*(row_pointers[y]+x) |= 1;
-			else
-				*(row_pointers[y]+x) &= 0xFE;
-		}
-		//Make sure that we did not use a file too large that it can't be encoded
-		if(y >= read_ptr->height)
-			exit(1);
+			//Make sure that we did not use a file too large that it can't be encoded
+			if(y >= read_ptr->height)
+				exit(1);
 	}
 
 	//goto jumps here to break out of multiple loops
-	loop_end:
+	//loop_end:
 
 	fclose(fileToEncode);
 
--- PNG_encode_decode/main.cpp
+++ PNG_encode_decode/main.cpp
@@ -1,8 +1,10 @@
 #include "PNG_file.h"
-void main() {
+
+void main(int argc, char *argv[])
+{
 	PNG_file link = PNG_file("big.png");
 	link.encode("1.txt");
 	link.outputPNG("bigen.png");
-//	PNG_file link = PNG_file("bigensimple.png");
-//	link.decode("2.txt");
+	PNG_file link2 = PNG_file("bigen.png");
+	link2.decode("2.txt");
 }
